0a2723978de83fa9395959b0afdc89682c1d9011
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2024-02-01 00:43:29 +0100

    minor fix

--- pages/nft/create.vue
+++ pages/nft/create.vue
@@ -417,7 +417,7 @@ export default {
 
           const options = {
             body: "I have launched a new NFT collection: " + this.cName + " <br /><br />Check it out here 👇", 
-            context: this.$config.chatChannels.general,
+            context: this.$config.orbisContext,
             data: {
               type: "nftCollectionCreated",
               authorAddress: String(this.address),
